0475428b1f0f019a2c2e6118e06078384592a265
lmobj <lmobj@cn.ibm.com>
2017-10-24 09:52:28 -0800

    Move Optimization out

--- Engine/RabbitmqHandler.cs
+++ Engine/RabbitmqHandler.cs
@@ -6,10 +6,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuantConnect.Optimization
+namespace QuantConnect.Lean.Engine
 {
     class RabbitmqHandler
     {
+        /*
         public void Publish(Algorithm.CSharp.model.Parameters parameters)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
@@ -41,6 +42,36 @@ namespace QuantConnect.Optimization
             }
 
         }
+        */
 
+        public void Publish(string statistics, string queueName)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Declaring a queue is idempotent - it will only be created if it doesn't exist already
+                channel.QueueDeclare(queue: queueName,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                // this will mark the msg persistent by writing it to cache and disk
+                properties.Persistent = true;
+
+                // create the message; msg is a byte array, encode whatever u like
+                var body = Encoding.UTF8.GetBytes(statistics);
+
+                // publish is the key action for producer; send to the queue
+                // the task will only send once. once the task is sent to the queue, it will out of the channel.
+                channel.BasicPublish(exchange: "",
+                                     routingKey: queueName,
+                                     basicProperties: properties,
+                                     body: body);
+                Log.Trace("Published a worker's log to " + queueName + ": " + statistics);
+            }
+        }
     }
 }
