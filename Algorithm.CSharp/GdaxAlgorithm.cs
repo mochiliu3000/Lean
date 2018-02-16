f6424ee46f647c817cc0ff15e48b638cdf73fb95
mochiliu3000 <mochiliu3000@gmail.com>
2018-02-16 20:07:38 +0800

    Add gdax algo

---
+++ Algorithm.CSharp/GdaxAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class GdaxAlgorithm : QCAlgorithm
+    {
+        private Symbol _spy = QuantConnect.Symbol.Create("BTCUSD", SecurityType.Crypto, Market.GDAX);
+
+        public override void Initialize()
+        {
+            AddCrypto("BTCUSD", Resolution.Tick, Market.GDAX);
+        }
+        public override void OnData(Slice data)
+        {
+            Console.WriteLine(data[_spy].ticks[0].Values[0]);
+        }
+    }
+}
